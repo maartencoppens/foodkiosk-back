d6a4a1489dfb0b63f4e09abaf4ac92a257a23061
Maarten Coppens <maarcopp@student.arteveldehs.be>
2025-05-09 12:06:47 +0200

    error update

--- server/services/db.ts
+++ server/services/db.ts
@@ -1,11 +1,11 @@
 import 'dotenv/config';
 import postgres, { Sql } from "postgres";
 
-if (!process.env.DATABASE_URL) {
+if (!process.env.POSTGRES_URL) {
   throw new Error("DATABASE_URL environment variable is not set");
 }
 
-const sql = postgres(process.env.DATABASE_URL, {
+const sql = postgres(process.env.POSTGRES_URL, {
   ssl: "require", // nodig voor Supabase
 });
 
